17319ffcffef1d62f46ab39776485ba05ab82900
Vijay SB <vijaysb2006@gmail.com>
2024-12-04 23:07:22 +0530

    Added TabBar

--- chronos-app/app/(tabs)/_layout.tsx
+++ chronos-app/app/(tabs)/_layout.tsx
@@ -1,43 +1,47 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+        tabBarShowLabel: false,
+        tabBarStyle: {
+        justifyContent:'center', 
+        backgroundColor: 'white',
+        height: 45, 
+        },
+        // tabBarActiveTintColor: '#3572EF', 
+        // tabBarInactiveTintColor: '#FFFFFF',
+      }}
+    >
       <Tabs.Screen
-        name="index"
+        name="Home"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="home" size={24} style={{ color, textAlign: 'center' }} />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="Activity"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="clipboard-clock" size={24} style={{ color, textAlign: 'center' }} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Admin"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome6 name="user-circle" size={24} style={{ color, textAlign: 'center' }} />
+          ),
         }}
       />
     </Tabs>
