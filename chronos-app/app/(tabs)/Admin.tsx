edc1eb14fe3a92abf36d596ec38ebcbc15e97550
Vijay SB <vijaysb2006@gmail.com>
2024-12-09 21:48:52 +0530

    Added top bg image

--- chronos-app/app/(tabs)/Admin.tsx
+++ chronos-app/app/(tabs)/Admin.tsx
@@ -1,26 +1,32 @@
-import '../../global.css'
-import { View, Text,Image, ImageBackground } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import '../../global.css';
+import { View, Text, ImageBackground } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const imgUrl=require("../../assets/images/adminbg2.jpg")
+const imgUrl = require('../../assets/images/adminbg2.jpg');
+
 const Admin = () => {
   return (
-<SafeAreaView className='flex-1 bg-mybg'>
-  <View>
+    <SafeAreaView className="flex-1 bg-mybg">
+      <View className="h-1/4">
+        <ImageBackground
+          source={imgUrl}
+          className="flex-1 justify-center items-center"
+          resizeMode="cover"
+        >
+          <Text className="text-center text-white text-lg font-bold">
+            Hello Admin
+          </Text>
+          <Text className="text-center text-white text-lg font-bold">
+            Welcome!
+          </Text>
+        </ImageBackground>
+      </View>
+      <View className="flex-1 justify-center items-center">
+        <Text className="text-white text-lg">More Content Here</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-    <ImageBackground
-    source={imgUrl}
-    className=''
-    />
-   
-    <Text className='text-center text-white'>Hello Admin</Text>
-    <Text className='text-center text-white'>Hello</Text>
-  </View>
-</SafeAreaView>
-
-
-  )
-}
-
-export default Admin
+export default Admin;
