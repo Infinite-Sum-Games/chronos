3135e9f592d4445f7862f4cad846a83ba514df83
Vijay SB <vijaysb2006@gmail.com>
2024-12-13 16:08:25 +0530

    Style improvements

--- chronos-app/app/(tabs)/Admin.tsx
+++ chronos-app/app/(tabs)/Admin.tsx
@@ -4,53 +4,71 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CustomButtons from "@/components/AdminComponents/CustomButtons";
 import ProgressBar from "@/components/AdminComponents/ProgressBar";
-const imgUrl = require("../../assets/images/adminbg3.jpg");
-const hrurl=require("../../assets/images/hr1.jpg")
+const imgUrl = require("../../assets/images/adminbg6.jpg");
+const imgUrl2 = require("../../assets/images/adminfooter.png");
+
 const Admin = () => {
   return (
     <SafeAreaView className="flex-1 bg-mybg">
-      <View className="h-1/4">
+      <View className="h-[30%]">
         <ImageBackground
           source={imgUrl}
-          className="flex-1 justify-center items-center"
+          className="flex-1 justify-center"
           resizeMode="cover"
         >
-          <Text className="text-center text-white text-lg font-bold">
+          {/* <Text className=" text-white text-lg font-bold">
             Hello Admin
           </Text>
-          <Text className="text-center text-white text-lg font-bold">
+          <Text className=" text-white text-lg font-bold mb-24">
             Welcome!
-          </Text>
+          </Text> */}
         </ImageBackground>
       </View>
 
+      <View className="h-[30%] flex-col justify-center mt-4 gap-4">
 
-      
+  <View className="flex flex-row justify-center">
+    <View className="flex-1 items-center">
+      <CustomButtons title={"Create Event"} />
+    </View>
+    <View className="flex-1 items-center">
+      <CustomButtons title={"Create Event"} />
+    </View>
+    <View className="flex-1 items-center">
+      <CustomButtons title={"Create Event"} />
+    </View>
+    <View className="flex-1 items-center">
+      <CustomButtons title={"Create Event"} />
+    </View>
+  </View>
+  <View className="flex flex-row justify-center mt-3">
+    <View className="flex-1 items-center">
+      <CustomButtons title={"Create Event"} />
+    </View>
+    <View className="flex-1 items-center">
+      <CustomButtons title={"Create Event"} />
+    </View>
+    <View className="flex-1 items-center">
+      <CustomButtons title={"Create Event"} />
+    </View>
+    <View className="flex-1 items-center">
+      <CustomButtons title={"Create Event"} />
+    </View>
+  </View>
+</View>
 
 
-      <View className="h-1/4 flex-col justify-center">
-        <View className="flex flex-row justify-center">
-          <CustomButtons title={"Add Activity"} />
-          <CustomButtons title={"View Reports"} />
-          <CustomButtons title={"Manage Users"} />
-          <CustomButtons title={"Settings"} />
-        </View>
-        <View className="flex flex-row justify-center">
-          <CustomButtons title={"Create Event"} />
-          <CustomButtons title={"Approve Requests"} />
-          <CustomButtons title={"Notifications"} />
-          <CustomButtons title={"Help"} />
-        </View>
-      </View>
-      <View className="border-b border-red-500 opacity-50" />
-      <View className="h-1/4 justify-center">
-        <Text className="text-white">Number Of Days Left</Text>
+      <View className="h-=[30%] flex justify-center">
+        <Text className="text-center text-white text-lg font-semibold mb-2 mt-2">
+          Progress Overview
+        </Text>
         <ProgressBar />
       </View>
-      <View className="border-b border-red-500 opacity-50" />
-      <View className="h-1/4">
+
+    
+      <View className="h-[30%] border-b">
         <ImageBackground
-          source={imgUrl}
+          source={imgUrl2}
           className="flex-1 justify-center items-center"
           resizeMode="cover"
         >
--- chronos-app/components/AdminComponents/CustomButtons.tsx
+++ chronos-app/components/AdminComponents/CustomButtons.tsx
@@ -1,20 +1,23 @@
-import { View, Text } from "react-native";
-import "../../global.css";
 import React from "react";
+import { View, Pressable, Text, StyleSheet } from "react-native";
 import AntDesign from "@expo/vector-icons/AntDesign";
-import { Pressable } from "react-native";
 interface CustomButtonsProps {
-  title: String;
+  title: string;
 }
+
 const CustomButtons: React.FC<CustomButtonsProps> = ({ title }) => {
   return (
-    <View className="flex justify-center items-center px-3 py-5">
-      <Pressable className="py-2 active:bg-gray-700  ">
-        <AntDesign name="swap" size={24} color="white" />
+    <View>
+      <Pressable className="justify-center items-center"
+        android_ripple={{ color: "rgba(255, 255, 255, 0.2)", borderless: false }}
+      >
+        <AntDesign name="swap" size={36} color="white" />
       </Pressable>
-      <Text className="text-white font-bold">{title}</Text>
+      <Text className="text-white text-center text-sm p-4 ">{title}</Text>
     </View>
   );
 };
+
+
 
 export default CustomButtons;
