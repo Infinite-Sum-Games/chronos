0ec5445303af0345c34756a32712aae3cad40424
Vijay SB <vijaysb2006@gmail.com>
2024-12-16 00:39:40 +0530

    Removed redundant code form FooterComponent

--- chronos-app/components/AdminComponents/CustomFooter.tsx
+++ chronos-app/components/AdminComponents/CustomFooter.tsx
@@ -5,10 +5,6 @@ import { Ionicons } from "@expo/vector-icons";
 const Footer = () => {
   return (
     <View className="bg-[#151515] px-6 py-8 rounded-t-3xl relative overflow-hidden border border-gray-700">
-      <View className="absolute inset-0">
-        <View className="absolute top-0 left-0 w-1/2 h-full bg-gradient-to-br from-[#1E293B] to-transparent opacity-20" />
-        <View className="absolute bottom-0 right-0 w-1/2 h-full bg-gradient-to-tl from-[#374151] to-transparent opacity-20" />
-      </View>
       <View className="items-center relative">
         <Text className="text-lg font-bold text-center text-gray-200 mb-4">
           Made by Infinite Sum Games
