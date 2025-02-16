65c2fcbe9e9b7130cda75a6c2231f58ba851c1d4
Nandgopal-R <nandgopalrnair@gmail.com>
2025-02-16 21:18:00 +0530

    Edited endpoint daily

--- supabase/functions/daily/index.ts
+++ supabase/functions/daily/index.ts
@@ -2,7 +2,7 @@ import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 
 import { Context, Hono } from "jsr:@hono/hono";
 import { db } from "../_shared/db.ts";
-import { timetable } from "../_shared/schema.ts";
+import { timetable, courses } from "../_shared/schema.ts";
 import { eq } from "https://esm.sh/drizzle-orm@0.30.10";
 
 const funcName = "daily";
@@ -12,7 +12,12 @@ const app = new Hono().basePath(`/${funcName}`);
 app.get("/", async (c: Context) => {
   try {
     const curday = new Date().toLocaleDateString("en-US", { weekday: "long" });
-    const data = await db.select().from(timetable).where(eq(timetable.day, curday));
+    const data = await db.select({
+      courseId: timetable.courseId,
+      courseName: courses.courseName,
+      slot: timetable.slotNum,
+      isElective: courses.isElective,
+    }).from(timetable).innerJoin(courses, eq(timetable.courseId, courses.courseId)).where(eq(timetable.day, curday));
     return c.json(data);
   } catch (error: unknown) {
     console.log(error);
