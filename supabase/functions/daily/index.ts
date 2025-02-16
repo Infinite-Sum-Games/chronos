35a2678e2ed769a4f5dfdbb0a596e154d2dac2a9
Nandgopal-R <nandgopalrnair@gmail.com>
2025-02-16 18:31:18 +0530

    Add daily endpoint and bruno folder

---
+++ supabase/functions/daily/index.ts
@@ -0,0 +1,23 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+
+import { Context, Hono } from "jsr:@hono/hono";
+import { db } from "../_shared/db.ts";
+import { timetable } from "../_shared/schema.ts";
+import { eq } from "https://esm.sh/drizzle-orm@0.30.10";
+
+const funcName = "daily";
+const app = new Hono().basePath(`/${funcName}`);
+
+
+app.get("/", async (c: Context) => {
+  try {
+    const curday = new Date().toLocaleDateString("en-US", { weekday: "long" });
+    const data = await db.select().from(timetable).where(eq(timetable.day, curday));
+    return c.json(data);
+  } catch (error: unknown) {
+    console.log(error);
+    return c.json({ message: "An unknown error occurred" }, 500);
+  }
+});
+
+Deno.serve(app.fetch);
