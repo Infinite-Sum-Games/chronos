6a300f669bafc0c2d5b027619ee91c1baf87eb26
Nandgopal-R <nandgopalrnair@gmail.com>
2025-02-16 19:01:17 +0530

    Edited error message and removed unnecessary dependancy

--- supabase/functions/daily/index.ts
+++ supabase/functions/daily/index.ts
@@ -12,11 +12,11 @@ const app = new Hono().basePath(`/${funcName}`);
 app.get("/", async (c: Context) => {
   try {
     const curday = new Date().toLocaleDateString("en-US", { weekday: "long" });
-    const data = await db.select().from(timetable).where(eq(timetable.day, curday));
+    const data = await db.select().from(timetable).where(eq(timetable.day, 'Monday'));
     return c.json(data);
   } catch (error: unknown) {
     console.log(error);
-    return c.json({ message: "An unknown error occurred" }, 500);
+    return c.json({ message: "Internal Server Error" }, 500);
   }
 });
 
