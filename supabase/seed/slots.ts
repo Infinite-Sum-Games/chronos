cc434a9336f2e2568f6f862fe9c14e0d359063df
Nandgopal-R <nandgopalrnair@gmail.com>
2025-02-17 19:08:51 +0530

    Minor fixes

--- supabase/seed/slots.ts
+++ supabase/seed/slots.ts
@@ -8,12 +8,12 @@ export default async function seedSlots(db: any) {
     { date: "2025-02-23", day: "Monday", slotNum: 1, courseId: "23MAT216", roomNo: "B201" },
     { date: "2025-02-24", day: "Tuesday", slotNum: 2, courseId: "22ADM201", roomNo: "C104" },
     { date: "2025-02-25", day: "Wednesday", slotNum: 3, courseId: "23CSE213", roomNo: "B303" },
-    { date: "2025-02-26", day: "Thurs", slotNum: 1, courseId: "23CSE211", roomNo: "A101" },
-    { date: "2025-02-27", day: "Thursday", slotNum: 2, courseId: "23CSE214", roomNo: "A103" },
-    { date: "2025-02-28", day: "Friday", slotNum: 3, courseId: "23CSE212", roomNo: "A101" },
-    { date: "2025-03-01", day: "Saturday", slotNum: 1, courseId: "23MAT216", roomNo: "B201" },
-    { date: "2025-03-02", day: "Monday", slotNum: 2, courseId: "22ADM201", roomNo: "C104" },
-    { date: "2025-03-03", day: "Tuesday", slotNum: 3, courseId: "23CSE213", roomNo: "B303" },
+    { date: "2025-02-26", day: "Thursday", slotNum: 1, courseId: "23CSE211", roomNo: "A101" },
+    { date: "2025-02-27", day: "Friday", slotNum: 2, courseId: "23CSE214", roomNo: "A103" },
+    { date: "2025-02-28", day: "Saturday", slotNum: 3, courseId: "23CSE212", roomNo: "A101" },
+    { date: "2025-03-01", day: "Monday", slotNum: 1, courseId: "23MAT216", roomNo: "B201" },
+    { date: "2025-03-02", day: "Tuesday", slotNum: 2, courseId: "22ADM201", roomNo: "C104" },
+    { date: "2025-03-03", day: "Wednesday", slotNum: 3, courseId: "23CSE213", roomNo: "B303" },
   ]);
   console.log("Slots seeded.");
 }
